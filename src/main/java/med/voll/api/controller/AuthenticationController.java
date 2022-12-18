14bcf1805de4ce096bd34d90f279de6f7de32a72
FelipeHiroshi13 <felipehiroshi13@gmail.com>
2022-12-18 01:00:46 -0400

    Authentication and Security

---
+++ src/main/java/med/voll/api/controller/AuthenticationController.java
@@ -0,0 +1,27 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.user.DataAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/login")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity login(@RequestBody @Valid DataAuthentication data){
+
+        var token = new UsernamePasswordAuthenticationToken(data.login(), data.login());
+        var authentication = manager.authenticate(token);
+
+
+        return ResponseEntity.ok().build();
+    }
+}
